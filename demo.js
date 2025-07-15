5032542d78195e7e92e124a260f5b1360b781f27
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-07-15 18:34:51 +0530

    Class Done

---
+++ demo.js
@@ -0,0 +1,23 @@
+class products{
+    // data member
+    name;
+    price;
+    category;
+    description;
+    rating;
+
+    // member fuction
+    addToCart(){
+        console.log("Product added."); 
+    }
+    removeFromCart(){
+        console.log("Product removed");
+    }
+    displayCart(){
+        console.log("Product displayed");
+    }
+    buyProduct(){
+        console.log("Product Bought")
+    }
+
+}
